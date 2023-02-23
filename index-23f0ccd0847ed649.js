4c235e377934a330fde7c55829e33fc20059ed5f
NSoiffer <NSoiffer@users.noreply.github.com>
2023-02-23 00:20:17 +0000

    update

--- index-23f0ccd0847ed649.js
+++ index-23f0ccd0847ed649.js
@@ -276,7 +276,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-882129a62b58921f_bg.wasm', import.meta.url);
+        input = new URL('index-23f0ccd0847ed649_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -652,8 +652,8 @@ async function init(input) {
         const ret = getObject(arg0).createElementNS(arg1 === 0 ? undefined : getStringFromWasm0(arg1, arg2), getStringFromWasm0(arg3, arg4));
         return addHeapObject(ret);
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper4586 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 220, __wbg_adapter_22);
+    imports.wbg.__wbindgen_closure_wrapper4587 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 216, __wbg_adapter_22);
         return addHeapObject(ret);
     };
 
